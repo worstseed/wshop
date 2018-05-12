615f94c54cfe576b2356eed7b14d3eeac4c97f3d
maciej <maciejanowski@gmail.com>
2018-05-12 17:56:44 +0200

    Script for creating the maze in minecraft world from geojson file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wshop
 minecraft fun
+
+1. Go to TeachCraft-Server run:
+	java -Xms1G -Xmx1G -jar Minecraft_Server.jar
+2. Go to TeachCraft-ClientLauncher run:
+	launcher.py
+3. Go to multiplayer mode & choose direct connect
+4. Go to "Work in Progress" run:
+	script.py
---
+++ Work in Progress/script.py
@@ -0,0 +1,72 @@
+########### LIBRARIES ############
+import mcpi.minecraft as minecraft
+import geojson
+import os
+##################################
+########## CONSTANTS #############
+cobblestone_block_id = 4
+
+default_name = "maciej"
+default_address = "127.0.0.1"
+default_height = 5
+
+maze_height = 11
+maze_width = 11
+
+dirpath = os.getcwd()
+maze_file = "maze-plan.geo.json"
+##################################
+
+# Connect to minecraft server 127.0.0.1 as player 'maciej' (default)!
+name = raw_input("Username (default: \"" + default_name + "\"): ")
+address = raw_input("Minecraft server address (default: " + default_address + "): ")
+
+# Set defaults.
+if not name:
+    name = default_name
+if not address:
+    address = default_address
+
+# Connect!
+mc = minecraft.Minecraft.create(address=address, name=name)
+
+# Set world spawn in the minecraft world.
+y = mc.getHeight(0, 0)
+print('In your Server console, please run the following command:')
+print('\tsetworldspawn 0 ' + str(y) + ' 0')
+
+# Get player's position.
+player_position = mc.player.getTilePos()
+
+# Get the plan of desired maze.
+with open(dirpath + "/" + maze_file) as input_file:
+    geojson_file = geojson.load(input_file)
+coordinates = geojson_file['coordinates']
+
+# Set how high the maze should be created in regard to player's position.
+height = raw_input("Set height above the ground (default: " + str(default_height) + "): ")
+if not height:
+    height = default_height
+
+# mc.setBlock(features[0], features[1], features[2], cobblestone_block_id)
+
+# Create stairs to the maze area.
+for i in range(0, height + 1):
+    mc.setBlock(player_position.x + 1 + i, player_position.y + i, player_position.z, cobblestone_block_id)
+
+# Create the maze area.
+for x in range(0, maze_height):
+    for z in range((maze_width - 1)/(-2), (maze_width + 1)/2):
+        mc.setBlock(player_position.x + height + 2 + x, player_position.y + height, player_position.z + z,
+                    cobblestone_block_id)
+        if x == 0 or x == (maze_height - 1) or z == (maze_width - 1)/(-2) or z == (maze_width - 1)/(2):
+            mc.setBlock(player_position.x + height + 2 + x, player_position.y + height + 1, player_position.z + z,
+                        cobblestone_block_id)
+
+# Build the actual maze.
+for c in coordinates:
+    mc.setBlock(player_position.x + height + 3 + c[0], player_position.y + height + 1, player_position.z + c[1] - 4,
+                cobblestone_block_id)
+
+# Move the player to starting position in the maze.
+mc.player.setPos(player_position.x + height + 4, player_position.y + height + 1, player_position.z + 0.5)
